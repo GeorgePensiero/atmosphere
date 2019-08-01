067c07a381a8ca5fee4912d28e672de51fa48d16
GeorgePensiero <georgepensiero@gmail.com>
2019-08-01 09:49:14 -0400

    reset tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_153836) do
+ActiveRecord::Schema.define(version: 2019_08_01_134445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
