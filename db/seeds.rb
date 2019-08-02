073eb6c2deb0928aa91e908bf88b23de449450c4
GeorgePensiero <georgepensiero@gmail.com>
2019-08-01 20:01:31 -0400

    change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.destroy_all
+Story.destroy_all
 
 User.create!(username:"HomerSimpson@nuclearplant.jpg", password: "123456")
 User.create!(username:"GobBluth@magicians.com", password: "123456")
