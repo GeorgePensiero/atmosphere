cb37acc5c378486b9427182e4dcbbdd02848d787
GeorgePensiero <georgepensiero@gmail.com>
2019-08-02 12:14:30 -0400

    made changes to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-<<<<<<< HEAD
-
-User.create!(username:"HomerSimpson", password: "123456")
-User.create!(username:"Gob Bluth", password: "123456")
-User.create!(username:"George", password: "123456")
-User.create!(username:"demo@demo.com", password: "password")
-=======
 Story.destroy_all
 
 User.create!(username:"HomerSimpson@nuclearplant.jpg", password: "123456")
@@ -27,4 +20,3 @@ Story.create!(title: "This is the demo story", body: "demoman gotta demo", autho
 Story.create!(title: "Got your nose, got your wallet", body: "doh", author_id: User.first.id)
 Story.create!(title: "I've made a terrible mistake", body: "I've made a terrible mistake", author_id: User.second.id)
 Story.create!(title: "How i learned to make chicken noises", body: "kawkawkawkawkaw", author_id: User.second.id)
->>>>>>> story
