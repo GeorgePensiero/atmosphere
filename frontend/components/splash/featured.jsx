f43f3073a19638393bb5766754a82a60288b0c94
GeorgePensiero <georgepensiero@gmail.com>
2019-08-04 19:46:22 -0400

    add icecream photos

--- frontend/components/splash/featured.jsx
+++ frontend/components/splash/featured.jsx
@@ -26,6 +26,7 @@ class Featured extends React.Component{
             const author = users[story.author_id].username.split("@")[0];
             return (
                 <div className="cover-story" key={story.id}>
+                    <img className="feature-cover-photos" src={story.photoUrl} alt="coverStoryPhoto"/>
                     <h1 className="cover-story-header">{story.title}</h1>
                     <Link to={`users/${users[story.author_id].id}/stories`}>{author}</Link>
                 </div>
